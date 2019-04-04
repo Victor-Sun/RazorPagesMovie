e51099dba2e439455a3d07e0c0d68571a5b0796e
Victor <vsun@ostusa.com>
2019-04-04 11:13:57 -0400

    V

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,14 +7,27 @@ namespace RazorPagesMovie.Models
 	public class Movie
 	{
 		public int ID { get; set; }
+
+		[StringLength(60, MinimumLength = 3)]
 		public string Title { get; set; }
 
-		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
+		[Display(Name = "Release Date")]
 		public DateTime ReleaseDate { get; set; }
-		public string Genre { get; set; }
 
+		[Range(1,100)]
+		[DataType(DataType.Currency)]
 		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+		public string Genre { get; set; }
+
+		[Required]
+		[StringLength(5)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+		public string Rating { get; set; }
 	}
 }
